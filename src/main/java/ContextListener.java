e026509805c9691f04b4833925610d644ad4cf00
srati15 <srati15@freeuni.edu.ge>
2019-05-25 14:34:16 +0400

    initial commit

---
+++ src/main/java/ContextListener.java
@@ -0,0 +1,31 @@
+import manager.DaoManager;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener()
+public class ContextListener implements ServletContextListener{
+
+    // Public constructor is required by servlet spec
+    public ContextListener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+      sce.getServletContext().setAttribute("manager", new DaoManager());
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page import="manager.DaoManager" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+<% DaoManager manager = (DaoManager) application.getAttribute("manager");
+%>
+</body>
+</html>
