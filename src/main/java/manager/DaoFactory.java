847bb7e4e27b58c1a8173081121183a6b311d946
lashamez <lashakapanadze50@gmail.com>
2019-06-17 19:03:59 +0400

    cache in threads

--- src/main/java/manager/DaoFactory.java
+++ src/main/java/manager/DaoFactory.java
@@ -5,6 +5,7 @@ import enums.DaoType;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 class DaoFactory {
     private static Map<DaoType, Dao > map;
@@ -14,11 +15,30 @@ class DaoFactory {
         map.put(DaoType.Quiz, new QuizDao(new QuestionDao(), new AnswerDao()));
         map.put(DaoType.Announcement, new AnnouncementDao());
         map.put(DaoType.FriendRequest, new FriendRequestDao());
-
-        map.values().stream().forEach(s->s.cache());
+        map.put(DaoType.TextMessage, new TextMessageDao());
+        cache();
     }
+
+    private static void cache() {
+        CountDownLatch latch = new CountDownLatch(map.size());
+        map.values().stream().forEach(dao -> {
+            new Thread(() -> {
+                dao.cache();
+                latch.countDown();
+            }).start();
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     static <E extends Dao> E dispatch(DaoType daoType) {
-        if (map.containsKey(daoType)) return (E) map.get(daoType);
+        if (map.containsKey(daoType)) {
+            assert map.get(daoType).getDaoType().equals(daoType);
+            return (E) map.get(daoType);
+        }
         throw new IllegalArgumentException("Dao not found for "+daoType.name());
     }
 }
